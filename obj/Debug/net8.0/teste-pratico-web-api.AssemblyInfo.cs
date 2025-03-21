5412b259e026db5e53129fd627889f22d9849f57
programedegraca <aprendaprogramardegraca@gmail.com>
2025-03-21 17:18:29 -0300

    Criando o arquivo .gitignore

--- obj/Debug/net8.0/teste-pratico-web-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/teste-pratico-web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste-pratico-web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b810b26ba7dd8d2a4ba862d521d646679f76893e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9f0abc08dcb02394cfcdaf302b718afd9d8176")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste-pratico-web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste-pratico-web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
